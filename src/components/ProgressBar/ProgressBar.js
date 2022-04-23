3925b0d6522afba2a93acb1275ed7858c78f3bf8
Yohan Lee <io.yohanlee@gmail.com>
2022-04-23 09:56:03 -0400

    Implement ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,40 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Container = styled.div`
+  width: 370px;
+  background-color: hsla(0, 0%, 50%, 0.15);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+  position: relative;
+  height: var(--height);
+`
+
+const RadioValue = styled.div`
+  position: absolute;
+  background-color ${COLORS.primary};
+  border-radius: 4px 0px 0px 4px;
+  top: var(--top);
+  left: var(--left);
+  height: var(--height);
+  width: var(--width);
+`
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const containerStyle = {
+     '--height': size === 'large' ? '24px' : size === 'medium' ? '12px' : '8px'
+  }
+  const radioStyle = {
+    '--top': size === 'large' ? '4px' : '0px',
+    '--left': size === 'large' ? '1px' : '0px',
+    '--height': size === 'large' ? '16px' : size === 'medium' ? '12px' : '8px',
+    '--width': value + '%'
+  }
+  return (
+    <Container style={containerStyle}>
+      <RadioValue style={radioStyle} value={value} />
+    </Container>
+  );
 };
 
 export default ProgressBar;
