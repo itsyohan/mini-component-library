6251a641a85a3725f3a1a7cb2ef659b8800a8e19
Yohan Lee <io.yohanlee@gmail.com>
2022-04-23 10:25:28 -0400

    Refacter per video solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,36 +6,39 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const Container = styled.div`
-  width: 370px;
   background-color: hsla(0, 0%, 50%, 0.15);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: 8px;
-  position: relative;
   height: var(--height);
+  padding: var(--padding);
+`
+
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 8px;
 `
 
 const Bar = styled.div`
-  position: absolute;
-  background-color ${COLORS.primary};
-  border-radius: var(--border-radius);
-  top: var(--top);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0px 0px 4px;
   height: var(--height);
   width: var(--width);
 `
 
 const ProgressBar = ({ value, size }) => {
   const containerStyle = {
-     '--height': size === 'large' ? '24px' : size === 'medium' ? '12px' : '8px'
+     '--height': size === 'large' ? '24px' : size === 'medium' ? '12px' : '8px',
+    '--padding': size === 'large' ? '4px 2px' : '0',
   }
   const radioStyle = {
-    '--top': size === 'large' ? '4px' : '0px',
     '--height': size === 'large' ? '16px' : size === 'medium' ? '12px' : '8px',
     '--width': value + '%',
-    '--border-radius': value > 98 ? '4px' : '4px 0px 0px 4px'
   }
   return (
     <Container style={containerStyle}>
-      <Bar style={radioStyle} value={value} />
+      <BarWrapper>
+        <Bar style={radioStyle} value={value} />
+      </BarWrapper>
       <VisuallyHidden>{value}</VisuallyHidden>
     </Container>
   );
