2cf5860af4f0a8bd6e74930a4c5b31656fc2c25d
Yohan Lee <io.yohanlee@gmail.com>
2022-04-23 20:41:09 -0400

    Icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,42 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+  position: relative;
+  padding-left: 30px;
+  height: var(--height);
+  border-bottom: var(--stroke) solid #000000;
+  width: var(--width);
+  color: hsla(0, 0%, 40%, 1);
+
+  &:focus-within {
+    outline: 2px solid hsla(218, 57%, 53%, 1);
+    border-radius: 2px;
+  }
+
+  &:hover {
+    color: hsla(0, 0%, 0%, 1);
+  }
+
+  input[type="text"] {
+    color: inherit;
+    border: none;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: ${p => p.size === 'large' ? 5 : 0}px;
+`
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +49,18 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const style = {
+    '--height': size === 'large' ? '36px' : '24px',
+    '--width': width + 'px',
+    '--stroke': size === 'large' ? '2px' : '1px',
+  }
+
+  return (
+    <Wrapper style={style} size={size}>
+      <IconWrapper size={size}><Icon id={icon}  /></IconWrapper>
+      <input type="text" placeholder={placeholder} />
+    </Wrapper>
+  )
 };
 
 export default IconInput;
